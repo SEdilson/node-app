211a79e12049ab6e489e3a869dce738b8e52228c
SEdilson <edilson.silva00@hotmail.com>
2020-03-17 14:25:24 -0300

    Modificando database

--- src/app/views/base/home/home.marko.js
+++ src/app/views/base/home/home.marko.js
@@ -1,16 +1,13 @@
-// Compiled using marko@4.13.4-1 - DO NOT EDIT
+// Compiled using marko@4.19.7 - DO NOT EDIT
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/casadocodigo$1.0.0/src/app/views/base/home/home.marko",
-    components_helpers = require("marko/src/components/helpers"),
-    marko_renderer = components_helpers.r,
-    marko_defineComponent = components_helpers.c,
-    marko_helpers = require("marko/src/runtime/html/helpers"),
-    marko_loadTag = marko_helpers.t,
-    component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
-    init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
-    await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
+    marko_componentType = "/node-app$1.0.0/src/app/views/base/home/home.marko",
+    marko_renderer = require("marko/src/runtime/components/renderer"),
+    marko_loadTag = require("marko/src/runtime/helpers/load-tag"),
+    component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
+    init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
 
 function render(input, out, __component, component, state) {
   var data = input;
@@ -33,13 +30,11 @@ marko_template._ = marko_renderer(render, {
     ___type: marko_componentType
   });
 
-marko_template.Component = marko_defineComponent({}, marko_template._);
-
 marko_template.meta = {
-    id: "/casadocodigo$1.0.0/src/app/views/base/home/home.marko",
+    id: "/node-app$1.0.0/src/app/views/base/home/home.marko",
     tags: [
-      "marko/src/components/taglib/component-globals-tag",
-      "marko/src/components/taglib/init-components-tag",
-      "marko/src/taglibs/async/await-reorderer-tag"
+      "marko/src/core-tags/components/component-globals-tag",
+      "marko/src/core-tags/components/init-components-tag",
+      "marko/src/core-tags/core/await/reorderer-renderer"
     ]
   };
