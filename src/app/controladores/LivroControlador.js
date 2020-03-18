3e4dcb2f4d8fa75087efe3ba18759113889c8a50
SEdilson <edilson.silva00@hotmail.com>
2020-03-17 22:22:00 -0300

    Adicionando controlador ao projeto

--- src/app/controladores/LivroControlador.js
+++ src/app/controladores/LivroControlador.js
@@ -1,10 +1,20 @@
+const db = require('../../config/database')
 const LivroDao = require('../infra/LivroDao')
+const { validationResult } = require('express-validator')
 
 class LivroControlador {
 
+    static rotas() {
+        return {
+            lista: '/livros',
+            cadastro: '/livros/form',
+            edicao: '/livros/form/:id',
+            delecao: '/livros/:id'
+        }
+    }
+
     lista() {
         return (req, resp) => {
-
             const livroDao = new LivroDao(db)
             livroDao.lista()
                     .then(books => {
@@ -16,6 +26,67 @@ class LivroControlador {
                         )        
                     })
                     .catch(error => console.log(error))
+        }
+    }
+
+    adicionaLivro() {
+        return (req, resp) => {
+            const errors = validationResult(req)
+            
+            if(!errors.isEmpty()) {
+                return resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { 
+                        livro: req.body,
+                        errosValidacao: errors.array()
+                    }
+                )
+            }
+            const livroDao = new LivroDao(db)
+            livroDao.adiciona(req.body)
+                    .then(resp.redirect(LivroControlador.rotas().lista))
+                    .catch(error => console.log(error))
+        }
+    }
+
+    atualizaLivro() {
+        return (req, resp) => {
+            console.log(req.body)
+            const livroDao = new LivroDao(db)
+            livroDao.atualiza(req.body)
+                    .then(resp.redirect(LivroControlador.rotas().lista))
+                    .catch(error => console.log(error))
+        }
+    }
+
+    buscaLivroPorId() {
+        return (req, resp) => {
+            const livroDao = new LivroDao(db)
+            livroDao.buscaPorId(req.params.id)
+                    .then(book => {
+                        resp.marko(
+                            require('../views/livros/form/form.marko'),
+                            {
+                               livro: book
+                            }
+                        )        
+                    })
+                    .catch(error => console.log(error))
+        }
+    }
+
+    removeLivro() {
+        return (req, resp) => {
+            const livroDao = new LivroDao(db)
+            livroDao.remove(req.params.id)
+                    .then(() => resp.status(200).end())
+                    .catch((error) => console.log(error))
+        }
+    }
+
+    exibeFormTemplate() {
+        return (req, resp) => {
+            resp.marko(require('../views/livros/form/form.marko'), { livro: {} })
         }
     }
 }
--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,68 +1,26 @@
-const LivroDao = require('../infra/LivroDao')
-const db = require('../../config/database')
-const { check, validationResult } = require('express-validator')
+const { check } = require('express-validator')
 
 const LivroControlador = require('../controladores/LivroControlador')
 const livroControlador = new LivroControlador()
 
+const BaseControlador = require('../controladores/BaseControlador')
+const baseControlador = new BaseControlador()
+
 module.exports = (app) => {
-    app.get('/', (req, resp) => {
-        resp.marko(require('../views/base/home/home.marko'))
-    })
+    app.get(BaseControlador.rotas().home, baseControlador.exibeHomeTemplate())
     
-    app.get('/livros', livroControlador.lista())
+    app.get(LivroControlador.rotas().lista, livroControlador.lista())
 
-    app.get('/livros/form', (req, resp) => {
-        resp.marko(require('../views/livros/form/form.marko'), { livro: {} })
-    })
+    app.get(LivroControlador.rotas().cadastro, livroControlador.exibeFormTemplate())
 
-    app.post('/livros', [
+    app.post(LivroControlador.rotas().lista, [
         check('titulo').isLength({ min: 5 }).withMessage('O título precisa ter no mínimo 5 caracteres'),
         check('preco').isCurrency().withMessage('O preço precisa ter um valor númerico')
-    ], (req, resp) => {
-        const errors = validationResult(req)
-        
-        if(!errors.isEmpty()) {
-            return resp.marko(
-                require('../views/livros/form/form.marko'),
-                { 
-                    livro: req.body,
-                    errosValidacao: errors.array()
-                }
-            )
-        }
-        const livroDao = new LivroDao(db)
-        livroDao.adiciona(req.body)
-                .then(resp.redirect('/livros'))
-                .catch(error => console.log(error))
-    })
+    ], livroControlador.adicionaLivro())
 
-    app.put('/livros', (req, resp) => {
-        console.log(req.body)
-        const livroDao = new LivroDao(db)
-        livroDao.atualiza(req.body)
-                .then(resp.redirect('/livros'))
-                .catch(error => console.log(error))
-    })
+    app.put(LivroControlador.rotas().lista, livroControlador.atualizaLivro())
 
-    app.get('/livros/form/:id', (req, resp) => {
-        const livroDao = new LivroDao(db)
-        livroDao.buscaPorId(req.params.id)
-                .then(book => {
-                    resp.marko(
-                        require('../views/livros/form/form.marko'),
-                        {
-                           livro: book
-                        }
-                    )        
-                })
-                .catch(error => console.log(error))
-    })
+    app.get(LivroControlador.rotas().edicao, livroControlador.buscaLivroPorId())
 
-    app.delete('/livros/:id', (req, resp) => {
-        const livroDao = new LivroDao(db)
-        livroDao.remove(req.params.id)
-                .then(() => resp.status(200).end())
-                .catch((error) => console.log(error))
-    })
+    app.delete(LivroControlador.rotas().delecao, livroControlador.removeLivro())
 }
